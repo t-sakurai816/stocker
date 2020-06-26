9e24e273499e5eb38f9b32fb257cbbff03c66d8e
root <root@ip-172-31-29-32.ap-northeast-1.compute.internal>
2020-06-26 10:45:04 +0000

    課題4-1クリア

--- insert_db.php
+++ insert_db.php
@@ -5,34 +5,33 @@ define('dsn', 'mysql:host=localhost; dbname=stock; charset=utf8');
 define('username', 'root');
 define('passwd', 'Hogehoge@1234');
 
-// 変数の初期化
-$sql = null;
-$res = null;
-$dbh = null;
-
-function addstock(){
+function addstock($name,$amount){
   try {
     // DBへ接続
     $dbh = new PDO(dsn, username, passwd);
-    echo "DB接続成功\n";
-  
-    // SQL作成
-    $sql = "INSERT INTO `management` (`id`, `name`) VALUES (NULL, 'db5')";
-  
-    // SQL実行
-    $res = $dbh->query($sql);
-    
-    // 確認メッセージ
-    echo "DBへ追加成功しました。\n";
-    echo $sql;
+    echo "DB接続成功";
+    echo "<br>";
   
   } catch(PDOException $e) {
     echo "接続失敗: " . $e->getMessage() . "\n";
     die();
   }
   
+  // SQL作成
+  $sql = "INSERT INTO `management` (`id`, `name`, `amount`) VALUES (NULL, '$name', $amount)";
+  $amount = 1; //デフォルト値(入力がなかった場合を1
+
+  // SQL実行
+  $res = $dbh->query($sql);
+  
+  // 確認メッセージ
+  echo "DBへ追加成功しました。";
+  echo "<br>";
+  echo $sql;
+  echo "<br>";
+  
   // 接続を閉じる
   $dbh = null;
 }
 
-addstock();
+addstock($_GET['name'],$_GET['amount']);
